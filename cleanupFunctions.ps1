3be18500b0cb0b109bfd1cea8d0736c0f8a7babd
jialutu <jialutu@gmail.com>
2016-11-17 09:56:59 +0000

    Update cleanupFunctions.ps1

--- cleanupFunctions.ps1
+++ cleanupFunctions.ps1
@@ -16,26 +16,21 @@ function Invoke-WeeklyCleanup
 	$path_wk = $path + '\weekly'
 
 	try {
-		if (Test-Path $path)
-		{
-			if ($week -eq $weekOfBackup)
-			{
+		if (Test-Path $path) {
+			if ($week -eq $weekOfBackup) {
 				$allDailyBackupFiles = Get-ChildItem -Path $path |?{!$_.PSIsContainer}
 				$latestBackupFile = $allDailyBackupFiles | Sort-Object -Property CreationTime -Descending | Select-Object -First 1
 				$file = $latestBackupFile.FullName
 				if (Test-Path $file)
 				{
-					if (Test-Path $path_wk)
-					{
+					if (Test-Path $path_wk) {
 						Move-Item -path $file -destination $path_wk
 					}
-					else
-					{
+					else {
 						throw "Weekly destination path does not exist"
 					}
 				}
-				else
-				{
+				else {
 					throw "Weekly backup does not exist"
 				}
 			}
@@ -44,13 +39,11 @@ function Invoke-WeeklyCleanup
 			
 			Get-ChildItem -Path $path_wk -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit_wk } | Remove-Item -Force
 		}
-		else
-		{
+		else {
 			throw "Path doesn't exist"
 		}
 	}
-	catch [Exception]
-	{
+	catch [Exception] {
 		throw "File clean up failed"
 	}
 }
@@ -67,23 +60,20 @@ function Invoke-Cleanup (
 	$limit = (Get-Date).AddDays($days)
 	
 	try {
-		if (Test-Path $path)
-		{
+		if (Test-Path $path) {
 			# Delete files older than the $limit.
 			Get-ChildItem -Path $path -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
 		}
-		else
-		{
+		else {
 			throw "Path doesn't exist"
 		}
 	}
-	catch [Exception]
-	{
+	catch [Exception] {
 		throw "File clean up failed"
 	}
 }
 
-function Invoke-Archive
+function Invoke-Archive 
 (
 	[Parameter(Mandatory=$true)]
 	[string]$path,
@@ -91,7 +81,7 @@ function Invoke-Archive
 	[int]$days = 30,
 	[Parameter(Mandatory=$false)]
 	[int]$days_arc = 600
-)
+) 
 {
 	$days = -1*$days
 	$days_arc = -1*$days_arc
@@ -103,32 +93,27 @@ function Invoke-Archive
 	$path_arc = $path + '\archive'
 
 	try {
-		if (Test-Path $path)
-		{
-			if (Test-Path $path_arc)
-			{	
+		if (Test-Path $path) {
+			if (Test-Path $path_arc) {	
 				Get-ChildItem -Path $path -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Move-Item -destination $path_arc
 			}
-			else
-			{
+			else {
 				mkdir $path_arc
 				Get-ChildItem -Path $path -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Move-Item -destination $path_arc
 			}
 		# Delete files older than the $limit_arc
 		Get-ChildItem -Path $path_arc -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit_arc } | Remove-Item -Force
 		}
-		else
-		{
+		else {
 			throw "Path doesn't exist"
 		}
 	}
-	catch [Exception]
-	{
+	catch [Exception] {
 		throw "File clean up failed"
 	}
 }
 
-function Invoke-Removal
+function Invoke-Removal 
 (
 	[Parameter(Mandatory=$true)]
 	[string]$path,
@@ -148,14 +133,24 @@ function Invoke-Removal
 		if (Test-Path $path)
 		{
 			Get-ChildItem -Path $path -Exclude $exclude -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
+			try {
+				$directories = Get-ChildItem -Path $path -Directory -Recurse -Force | Select-Object FullName
+				foreach ($directory in $directories) {
+					$count = Get-ChildItem -Path $directory.FullName | Measure-Object
+					if ($count.count -eq 0) {
+						Remove-Item -Path $directory.FullName -Recurse -Force
+					}
+				}
+			}
+			catch [Exception] {
+				throw "Empty directory removal failed"
+			}
 		}
-		else
-		{
+		else {
 			throw "Path doesn't exist"
 		}
 	}
-	catch [Exception]
-	{
+	catch [Exception] {
 		throw "File clean up failed"
 	}
 }
